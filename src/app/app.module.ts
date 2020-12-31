c01364cffdcee2a36f63bf6df2f0ff0f44bfa888
oswaldo olea <olea.oswaldo@gmail.com>
2020-12-31 18:56:39 +0100

    toolbar mat-buttons, routerLinkActive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,14 +8,19 @@ import { UsersCodesComponent } from './users/users-codes/users-codes.component';
 import { UsersDataComponent } from './users/users-data/users-data.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { TestComponent } from './test/test.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { TestsComponent } from './tests/tests.component';
+import { FlexLayoutModule } from '@angular/flex-layout';
 import { MatTableModule } from '@angular/material/table';
 import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatSortModule } from '@angular/material/sort';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { TestsComponent } from './tests/tests.component';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
-import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import {MatListModule} from '@angular/material/list';
+
 
 @NgModule({
   declarations: [
@@ -37,6 +42,10 @@ import { FlexLayoutModule } from '@angular/flex-layout';
     FormsModule,
     MatFormFieldModule,
     MatInputModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatListModule,
+    MatButtonModule,
     FlexLayoutModule
   ],
   providers: [],
